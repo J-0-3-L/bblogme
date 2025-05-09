ba100959759748d774d451044b09a4631c52af41
Joel Lawi <abecedegluu@gmail.com>
2025-05-08 21:02:29 -0500

    Agregando campo slug

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,8 +3,10 @@ import { defineCollection, z } from 'astro:content';
 const blogCollection = defineCollection({
   schema: z.object({
     title: z.string(),
+    info: z.string(),
     date: z.date(),
-    tags: z.array(z.string()).optional()
+    tags: z.array(z.string()).optional(),
+    slug: z.string()
   })
 })
 
